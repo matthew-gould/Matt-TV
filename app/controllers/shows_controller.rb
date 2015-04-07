299a7d61f6c670552a60d3f4547bda07da266fe2
matthew gould <gouldm44@gmail.com>
2015-04-07 13:21:22 -0400

    Added sortable tables

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,10 +1,21 @@
 class ShowsController < ApplicationController
+  helper_method :sort_column, :sort_direction
 
   def show
   end
 
   def index
-    @shows = Show.all
+    @shows = Show.order(sort_column + " " + sort_direction)
     @favorites = current_user.favorites.pluck(:show_id)
+  end
+
+  private
+  
+  def sort_column
+    Show.column_names.include?(params[:sort]) ? params[:sort] : "name"
+  end
+  
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
 end
